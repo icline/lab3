d3a48b270f91cae39271c35c1962764d2381affe
icline <94770492+icline@users.noreply.github.com>
2025-04-09 14:27:28 +0000

    Added Portia's changes.

--- main.py
+++ main.py
@@ -50,8 +50,10 @@ def main(letters, frequencies, output_file):
         traverse_duration = (stop_time - start_time) * 1000
         build_duration = (build_stop_time - build_start_time) * 1000
         output_file.write('\n\n')
-        output_file.write(f'Tree Traversal Duration: {traverse_duration:.4f} ms\n')
-        output_file.write(f'Build Tree Duration: {build_duration:.4f} ms\n')
+        output_file.write(f"Tree Traversal Duration: " + 
+                          f"{traverse_duration:.4f} ms\n")
+        output_file.write(f'Build Tree Duration: ' 
+                          f'{build_duration:.4f} ms\n')
         output_file.write('---------------------------------------------\n')
 
         # Print the Codes Generated from the Huffman Tree
@@ -90,7 +92,8 @@ def main(letters, frequencies, output_file):
             if not (is_alphabetic or is_binary):
                 output_file.write(f'Input: {line}\n')
                 output_file.write(f"Error: Invalid characters\n")
-                output_file.write('\n---------------------------------------------\n')
+                output_file.write(f'\n-------------------' 
+                                  f'--------------------------\n')
                 continue
 
             # Encode if line is alphabetic
@@ -103,7 +106,8 @@ def main(letters, frequencies, output_file):
                 if encoded['valid'] is False:
                     output_file.write(f'Input: "{line}"\n')
                     output_file.write(encoded['message'])
-                    output_file.write('\n---------------------------------------------\n')
+                    output_file.write(f'\n----------------------------' 
+                                      f'-----------------\n')
                     continue
 
                 # Calculate encoding time duration in milliseconds
@@ -117,30 +121,35 @@ def main(letters, frequencies, output_file):
                 Number of original bits (5 bits per character) divided by 
                 Number of post-encoded compressed bits
 
-                    - Since we are only generating codes for the letters in the alphabet,
-                    5 bits is the minimum necessary to code the 26 letters. In essence,
-                    using standard methods, the code lengths of each letter would be fixed
-                    at 5. However, Huffman encoding generates variable length codes that
+                    - Since we are only generating codes for the letters in 
+                    the alphabet, 5 bits is the minimum necessary to code the 
+                    26 letters. In essence, using standard methods, the code 
+                    lengths of each letter would be fixed at 5. However, 
+                    Huffman encoding generates variable length codes that
                     will reduce the length of the encoded string.
                 
                 Compression Rate Formula Reference: 
-                `A Comparison of Huffman Codes Across Ian 
-                <https://jhu.instructure.com/courses/93679/pages/module-8-huffman-tree-review-article?module_item_id=4291887>`_
+                'A Comparison of Huffman Codes Across Ian': 
+                (jhu.instructure.com/courses/93679/pages/
+                module-8-huffman-tree-review-article)
                 '''
                 original_enc_bits = len(line) * 5
                 compressed_bits = len(encoded['message'])
-                compression_rate = (1 - (compressed_bits/original_enc_bits)) * 100 
+                compression_rate = \
+                    (1 - (compressed_bits/original_enc_bits)) * 100 
 
                 total_enc_bits += original_enc_bits
                 total_compressed_bits += compressed_bits
 
                 # Print Encoding duration and Compression Rate metrics to 
                 # output file
-                output_file.write(f"Encoded:\n '{line}'\n to:\n {encoded['message']}\n")
+                output_file.write(f"Encoded:\n '{line}'\n to:\n" 
+                                  f" {encoded['message']}\n")
                 output_file.write(f"Duration: {duration:.4f} milliseconds\n")
                 output_file.write(f"Compression Rate: " 
                                   f"{compression_rate:.2f}%\n")
-                output_file.write('\n---------------------------------------------\n')
+                output_file.write(f'\n------------------------------------' 
+                                  f'---------\n')
                 encoded_count += 1
 
             # Decode if line consists of valid binary codes            
@@ -153,7 +162,8 @@ def main(letters, frequencies, output_file):
                 if decoded['valid'] is False:
                     output_file.write(f'Input: "{line}"\n')
                     output_file.write(decoded['message'])
-                    output_file.write('\n---------------------------------------------\n')
+                    output_file.write('\n--------------------------------' 
+                                      f'-------------\n')
                     continue
 
                 # Calculate decoding duration for line
@@ -162,7 +172,7 @@ def main(letters, frequencies, output_file):
                 total_duration += duration
 
                 '''
-                Calculate Decoding Decompression (Expansion) Rate: 
+                Calculate Decoding Decompression Rate: 
                 Number of decompressed bits (num characters * 5) divided by 
                 Number of original bits in binary codes line
                 '''
@@ -176,35 +186,38 @@ def main(letters, frequencies, output_file):
 
                 # Print Decoding duration and Decompression Rate metrics to 
                 # output file
-                output_file.write(f"Decoded:\n '{line}'\n to:\n {decoded['message']}\n")
+                output_file.write(f"Decoded:\n '{line}'\n to:\n " 
+                                  f"{decoded['message']}\n")
                 output_file.write(f"Duration: {duration:.4f} milliseconds\n")
-                output_file.write(f"Decompression (Expansion) Rate: "+ 
+                output_file.write(f"Decompression Rate: "+ 
                                   f"{decompression_rate:.2f}%\n")
-                output_file.write('\n---------------------------------------------\n')
+                output_file.write(f'\n----------------------------' 
+                                  f'-----------------\n')
                 decoded_count += 1
 
         # Calculate Overall Compression Rate for lines requiring encoding
-        overall_compression_rate = (total_compressed_bits / 
-                                    total_enc_bits) * 100 
+        overall_compression_rate = (1 - (total_compressed_bits / 
+                                    total_enc_bits)) * 100 
         # Print compression statistics for encoding
-        output_file.write(f"Total Original Bytes to Encode: " 
-                          f"{int(total_enc_bits/8)}\n")
-        output_file.write(f"Total Original Bits to Encode: {total_enc_bits}\n")
+        output_file.write(f"Total Original Letters for Encoding: " 
+                          f"{int(total_enc_bits/5)}\n")
+        output_file.write(f"Total Original Bits for Encoding: " 
+                          f"{total_enc_bits}\n")
         output_file.write(f"Total Compressed Bits: {total_compressed_bits}\n")
         output_file.write(f"Overall Compression Rate: " + 
                           f"{overall_compression_rate:.2f}%\n\n")
 
         # Calculate Overall Decompression Rate for lines requiring decoding       
-        overall_decompression_rate = (total_decompressed_bits / 
-                                      total_dec_bits) * 100 
+        overall_decompression_rate = (1 - (total_decompressed_bits / 
+                                      total_dec_bits)) * 100 
         # Print overall decompression statistics for decoding        
-        output_file.write(f"Total Original Bits to Decode: " 
+        output_file.write(f"Total Original Bits for Decoding: " 
                           f"{total_dec_bits}\n")
-        output_file.write(f"Total Final Decompressed Bytes: " + 
-                          f"{int(total_decompressed_bits/8)}\n")
+        output_file.write(f"Total Final Decompressed Letters: " + 
+                          f"{int(total_decompressed_bits/5)}\n")
         output_file.write(f"Total Final Decompressed Bits: " + 
                           f"{total_decompressed_bits}\n")
-        output_file.write(f"Overall Decompression (Expansion) Rate: " + 
+        output_file.write(f"Overall Decompression Rate: " + 
                           f"{overall_decompression_rate:.2f}%\n\n")
 
         # Print total duration of program 
@@ -213,30 +226,36 @@ def main(letters, frequencies, output_file):
         
         # Print total number of lines processed:
         total_lines_processed = encoded_count + decoded_count
-        output_file.write(f'Total Number of Lines Processed: {total_lines_processed}\n'
+        output_file.write(f'Total Number of Lines Processed: ' 
+                          f'{total_lines_processed}\n'
                           f'    - Lines Encoded: {encoded_count}\n'
                           f'    - Lines Decoded: {decoded_count}\n')
         input_file.close()
 
 if __name__ == "__main__":
     # Define two sets of letters and frequencies
-    letters_1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters_1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+                 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+                 'Y', 'Z']
     frequencies_1 = [19, 16, 17, 11, 42, 12, 14, 17, 16, 5, 10, 20, 19,
                      24, 18, 13, 1, 25, 35, 25, 15, 5, 21, 2, 8, 3]
     
-    letters_2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters_2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+                 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+                 'Y', 'Z']
     frequencies_2 = [10, 20, 30, 15, 25, 10, 35, 20, 18, 11, 16, 17, 14,
                      22, 19, 28, 24, 16, 20, 11, 23, 12, 13, 15, 10, 9]
 
     # Open output file in append mode to keep results from both runs
     with open('output.txt', 'w') as output_file:
-        # Loop through each pair of letters and frequencies and call the main function
+        # Loop through each pair of letters and frequencies and call the 
+        # main function
         i = 1
-        for letters, frequencies in [(letters_1, frequencies_1), (letters_2, frequencies_2)]:
+        for letters, frequencies in [(letters_1, frequencies_1), 
+                                     (letters_2, frequencies_2)]:
             # Write a separator between runs
-            output_file.write("\n-------------------------------------------------------------------------------\n")
+            output_file.write(f"\n----------------------------------------" 
+                              f"---------------------------------------\n")
             output_file.write(f'Frequency Table: {i}\n')
             output_file.write(f"Letters: {letters}\n")
             output_file.write(f"Frequencies: {frequencies}\n\n")
